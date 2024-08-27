6e8ad86fe3ad3ddd50f609d39d8b049677d3dc56
CEE <archmagece@users.noreply.github.com>
2024-08-27 19:26:46 +0900

    check: docker compose

--- configs/global_config.go
+++ configs/global_config.go
@@ -15,10 +15,16 @@ func (cfg *GlobalConfig) ReadConfig() {
 	confDir := helpers.GetEnv("CONFIG_DIR", "conf/")
 	helpers.ReadYaml(path.Join(confDir, "global.yaml"), cfg)
 
+	cfg.BaseDir = helpers.GetEnv("BASE_DIR", cfg.BaseDir)
 	if cfg.BaseDir == "" {
 		cfg.BaseDir = "~/tmp/deproxy"
 	}
 	if strings.HasPrefix(cfg.BaseDir, "~") {
+		//homeDir, err := os.UserHomeDir()
+		//if err != nil {
+		//	fmt.Printf("Error retrieving home directory: %v\n", err)
+		//	return
+		//}
 		cfg.BaseDir = helpers.ExpandHome(cfg.BaseDir)
 	}
 }
