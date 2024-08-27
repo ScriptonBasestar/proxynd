2a71a22237af2380e2575584c275d7d5e1d4738e
CEE <archmagece@users.noreply.github.com>
2024-08-27 16:48:44 +0900

    작업하다만거

---
+++ integration/README.md
@@ -0,0 +1,11 @@
+# Integration 테스트에서 할일
+
+- apt
+  - docker ubuntu 이미지 실행 후 mirror 설정해서 apt update apt upgrade
+    - 두번 연속 실행으로 빨라지는지 체크
+      1. 프록시 안쓰고 두번 실행
+      2. 프록시 쓰고 두번 실행
+
+- maven
+  - 패키지 다운로드
+  - 패키지 업로드
--- routers/proxy_router.go
+++ routers/proxy_router.go
@@ -6,24 +6,12 @@ import (
 )
 
 func ProxyRouter(r *gin.Engine) {
-
-	//proxy := goproxy.NewProxyHttpServer()
-	//proxy.Verbose = true
-	//log.Fatal(http.ListenAndServe(":8080", proxy))
-
-	//mavenGroup := r.Group("/repo")
-	//mavenGroup.GET("maven*", handlers.Proxy)
+	mavenGroup := r.Group("/proxy/repo")
+	mavenGroup.GET("*path", dproxy.Maven)
 	//r.GET("/repo/maven/:path", handlers.Proxy)
-	r.GET("/proxy/apt/*path", dproxy.Apt)
-	r.GET("/proxy/maven/*path", dproxy.Maven)
+	//r.GET("/proxy/maven/*path", dproxy.Maven)
+	aptGroup := r.Group("/proxy/apt")
+	aptGroup.GET("*path", dproxy.Apt)
+	//r.GET("/proxy/apt/*path", dproxy.Apt)
 	//r.GET("/proxy/vagrant/*path", func(context *gin.Context) {
-	//	proxy.OnRequest().DoFunc(
-	//		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-	//			if h, _, _ := time.Now().Clock(); h <= 8 && h <= 17 {
-	//				return r, goproxy.NewResponse(r, goproxy.ContentTypeText, http.StatusForbidden, "Don't waste your time!")
-	//			}
-	//			return r, nil
-	//		},
-	//	)
-	//})
 }
